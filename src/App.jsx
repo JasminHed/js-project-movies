29c46a8972c7952ff45845eebdc197f83bc828a8
Jasmin Hedlund <jasminhedlund@gmail.com>
2025-08-01 12:13:05 +0200

    updated hidden h1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styled from "styled-components"
 
 import MovieDetail from "./pages/MovieDetail";
 import Movies from "./pages/Movies.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import GlobalStyle from "./styling/GlobalStyle.jsx";
 
+const HiddenHeading = styled.h1`
+  position: absolute;
+  left: -9999px;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+`;
+
 export const App = () => {
   return (
     <>
-    <h1 className="sr-only">Movie Database App</h1>
+      <HiddenHeading>Movie Database App</HiddenHeading>
       <BrowserRouter>
         <GlobalStyle />
         <Routes>

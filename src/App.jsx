b0f8802c9dc5e7eae866c23efffd1994a1da78a7
Jasmin Hedlund <jasminhedlund@gmail.com>
2025-08-01 12:08:13 +0200

    added a hidden H1 in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import GlobalStyle from "./styling/GlobalStyle.jsx";
-import Movies from "./pages/Movies.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import MovieDetail from "./pages/MovieDetail";
+import Movies from "./pages/Movies.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import GlobalStyle from "./styling/GlobalStyle.jsx";
 
 export const App = () => {
   return (
     <>
+    <h1 className="sr-only">Movie Database App</h1>
       <BrowserRouter>
         <GlobalStyle />
         <Routes>
